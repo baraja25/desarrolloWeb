ef31d213888529a698e1e25216621e49ad22f458
baraja25 <92746513+baraja25@users.noreply.github.com>
2024-09-20 17:51:56 +0200

    ejercicio terminado

--- servidor/tema1/ejemplos/ajedrez.php
+++ servidor/tema1/ejemplos/ajedrez.php
@@ -1,12 +1,25 @@
 <html>
     <body>
         
-        
-        
+        <table border="2" width="500px" height="500px">
         <?php
-
+        for ($i=0; $i < 8; $i++) {
+            echo "<tr></tr>"; 
+            for ($j=0; $j < 4; $j++) { 
+                
+                if ($i % 2==0) {
+                    echo "<td bgcolor='black'></td><td bgcolor='white'></td>";
+                } else {
+                    echo "<td bgcolor='white'></td><td bgcolor='black'></td>";
+                }
+                 
+            }
+        }
 
 
         ?>
+        </table>
+        
+       
     </body>
 </html>
