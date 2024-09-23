eae4164cccaf827b00f2884e835d392d0fcb5d46
baraja25 <92746513+baraja25@users.noreply.github.com>
2024-09-23 18:00:52 +0200

    no se

--- servidor/tema1/ejemplos/arrays1.php
+++ servidor/tema1/ejemplos/arrays1.php
@@ -5,31 +5,11 @@
 
 
     <?php
-/* 
-    //mostrar elemento de un array unidimensional
 
-    $numeros = array(1, 2, 3, 4, 5, 6);
+$relleno=rand(1,20);
 
 
-    //array dinamico? añade elementos en un array vacio
-    $elementos=array();
 
-    //se puede dejar los [] vacios y se ira rellenando automaticamente
-    $elementos[4]=11;
-    //el relleno automatico se salta los indices si no se introduce nada
-    $elementos[]=2;
-    $elementos[]="lol";
-    $elementos[]=5;
-    $elementos[]=6;
-
-
-    foreach ( $elementos as $key => $value ) {
-        echo "El numero en la posicion $clave es $valor <br>";
-    } 
-*/
-
-
-//$edades=array('juan'=>19,'pepe'=>20,'mario'=>21,'lucas'=>22 );
 //esta es la forma que se suele trabajar los arrays
 $edades=array();
 $edades['juan']=19;
@@ -44,7 +24,9 @@ foreach ($edades as $key => $value) {
 }
 
 
+//rellenar 20 elementos un array 1 al 20, mostrar el array y decir el mayor, array minimo 10 maximo 20 despegable, numero 1-30
 
+//crear otro despegable => numero de repeticiones (1-5). Mostrar los numeros que se repitan X veces
 
 
     ?>
