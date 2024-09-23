4ee3670d4dff4f57f8a32e3460c42f5fb7393edd
baraja25 <92746513+baraja25@users.noreply.github.com>
2024-09-23 16:43:47 +0200

    forma de trabajo

--- servidor/tema1/ejemplos/arrays1.php
+++ servidor/tema1/ejemplos/arrays1.php
@@ -5,7 +5,7 @@
 
 
     <?php
-
+/* 
     //mostrar elemento de un array unidimensional
 
     $numeros = array(1, 2, 3, 4, 5, 6);
@@ -26,8 +26,22 @@
     foreach ( $elementos as $key => $value ) {
         echo "El numero en la posicion $clave es $valor <br>";
     } 
+*/
 
 
+//$edades=array('juan'=>19,'pepe'=>20,'mario'=>21,'lucas'=>22 );
+//esta es la forma que se suele trabajar los arrays
+$edades=array();
+$edades['juan']=19;
+$edades['pepe']=20;
+$edades['mario']=22;
+$edades['lucas']=49;
+
+
+foreach ($edades as $key => $value) {
+    echo "El alumno $key tiene $value años";
+    echo "<br>";
+}
 
 
 
