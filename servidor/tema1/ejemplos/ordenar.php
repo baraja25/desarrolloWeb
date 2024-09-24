d01a2e77b508788e146f09ea7f426f65352ae9a1
baraja25 <92746513+baraja25@users.noreply.github.com>
2024-09-24 16:08:07 +0200

    mas ejemplos de sorts

--- servidor/tema1/ejemplos/ordenar.php
+++ servidor/tema1/ejemplos/ordenar.php
@@ -2,27 +2,53 @@
 
 <body>
     <?php
-    $numeros = array(5, 7, 3, 1, 6, 9, 6, 5, 3, 2);
+    //pagina 157 para ver las funciones de el lenguaje PHP.pdf
+    $edades = array();
+    $edades["Jose"]=21;
+    $edades["juan"]=19;
+    $edades["luis"]=15;
+    $edades["maria"]=65;
+    $edades["Angel"]=40;
+
 
     echo "Antes de ordenar";
-    foreach ($numeros as $key => $value) {
-        echo " $value";
-    }
-    echo "<br>";
-    sort($numeros); //ordenacion por valor ascendente
-
-
-    echo "Ordenar ascendente";
-    foreach ($numeros as $key => $value) {
-        echo " $value";
+    foreach ($edades as $key => $value) {
+        echo "<br>Clave: $key valor: $value ";
     }
     echo "<br>";
 
-    rsort($numeros);
+    // sort($edades); //ordenacion por valor ascendente
 
-    echo "ordenar descendente";
-    foreach ($numeros as $key => $value) {
-        echo " $value";
+
+    // echo "Ordenar ascendente";
+    // foreach ($edades as $clave => $value) {
+    //     echo "<br>Clave: $clave valor: $value ";
+    // }
+    // echo "<br>";
+
+    // rsort($edades);
+
+    // echo "ordenar descendente";
+    // foreach ($edades as $key => $value) {
+    //     echo "<br>Clave: $key valor: $value ";
+    // }
+    // echo "<br>";
+
+    
+    //mantienen asociacion de clave y valor
+    asort($edades);
+
+    echo "ordenar con asort()";
+    foreach ($edades as $key => $value) {
+        echo "<br>Clave: $key valor: $value ";
+    }
+    echo "<br>";
+
+    ksort($edades);
+
+    echo "ordenar con ksort()";
+    foreach ($edades as $key => $value) {
+        echo "<br>Clave: $key valor: $value ";
     }
     echo "<br>";
 
