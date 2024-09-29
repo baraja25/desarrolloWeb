2b8dc1f030949460172b18b14e4709aba103133c
baraja25 <92746513+baraja25@users.noreply.github.com>
2024-09-29 18:11:22 +0200

    ejercicios de string terminados

--- cliente/tema1/ejercicios/cadenas.js
+++ cliente/tema1/ejercicios/cadenas.js
@@ -3,7 +3,6 @@ let texto = "Javascript es divertido!";
 let mayusculas = texto.toUpperCase();
 let minusculas = texto.toLowerCase();
 
-
 console.log(mayusculas);
 console.log(minusculas);
 
@@ -21,8 +20,6 @@ console.log(indiceLetra);
 
 let frase = "Aprendiendo javascript es muy interesante";
 
-
-
 let palabra = frase.slice(12, 22);
 
 console.log(palabra);
@@ -38,15 +35,69 @@ let palabras = "rojo,azul,verde,amarillo";
 palabra = palabras.split(",");
 
 for (let index = 0; index < palabra.length; index++) {
-    const element = palabra[index];
-    console.log(element);
+  const element = palabra[index];
+  console.log(element);
 }
 
 frase = palabra.join(",");
 
 console.log(frase);
 
+/* Ejercicio 5 */
+
+frase = "Javascript es increible, me encanta Javascript";
+
+let nuevaFrase = frase.replace("Javascript", "Python");
+
+console.log(nuevaFrase);
+
+nuevaFrase = frase.replaceAll("Javascript", "Python");
+
+console.log(nuevaFrase);
+
+/* Ejercicio 6 */
+
+let parte1 = "javascript";
+let parte2 = " es";
+let parte3 = " asombroso";
+
+let frase2 = parte1.concat(parte2, parte3);
+
+console.log(frase2);
+
+/* Ejercicio 7 */
+
+let nombre = " Alice ";
+
+nombre = nombre.trim();
+
+console.log(nombre);
+
+/* Ejercicio 8 */
+
+let oracion = "Me gusta aprender javascript";
+
+let encontrado = oracion.includes("javascript");
+
+console.log("javascript: ", encontrado);
+
+encontrado = oracion.includes("python");
+
+console.log("python: ",encontrado);
 
 
+/* Ejercicio 9 */
 
+let mensaje = "Bienvenido a la clase de javascript";
 
+console.log("Empieza por bienvenido: ",mensaje.startsWith("Bienvenido"));
+
+console.log("Termina con javascript: ", mensaje.endsWith("javascript"));
+
+/* Ejercicio 10 */
+
+palabra = "hola";
+
+let repe = palabra.repeat(3);
+
+console.log(repe);
