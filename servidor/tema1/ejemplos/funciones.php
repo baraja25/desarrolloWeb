4a87e65cc76eb0ba3f7c2fec8fcefbf261b7cf87
baraja25 <92746513+baraja25@users.noreply.github.com>
2024-09-30 16:42:24 +0200

    uso variable global

--- servidor/tema1/ejemplos/funciones.php
+++ servidor/tema1/ejemplos/funciones.php
@@ -1,19 +1,36 @@
 <?php
 $num = 10;
-function Ejemplo() {
+// function Ejemplo() {
     
-    global $num; //accede a la variable globl
+//     global $num; //accede a la variable globl
 
-    for ($i=0; $i < $num; $i++) { 
-        echo "Hola";
+//     for ($i=0; $i < $num; $i++) { 
+//         echo "Hola";
+//     }
+    
+    
+
+
+
+// }
+
+$potencia=1;
+
+function potencia($base, $exponente) {
+    global $potencia;
+
+    for ($i=0; $i < $exponente; $i++) { 
+        $potencia*=$base;
     }
-    
-    
-
-
-
 }
 
-Ejemplo();
+$base=2;
+$exponente=3;
+
+potencia($base, $exponente);
+
+echo "el resultado de la potencia de $base elevado a $exponente es: $potencia";
+
+//Ejemplo();
 
 ?>
