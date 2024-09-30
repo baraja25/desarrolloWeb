1a79675633114026d0a4fe33df1bc46ce831530d
baraja25 <92746513+baraja25@users.noreply.github.com>
2024-09-30 15:56:16 +0200

    ejemplo de funciones

---
+++ servidor/tema1/ejemplos/inArray.php
@@ -0,0 +1,12 @@
+<?php
+
+$notasalumnos=array("juan"=>6, "luis"=>4, "maria"=>9);
+
+//in_array(); Busqueda por valor
+
+//echo array_search("luis", $alumnos); la clave asociada a un valor
+
+
+
+
+?>
