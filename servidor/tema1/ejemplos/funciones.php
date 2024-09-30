ff2638a666123502e143fb771afe7320876f45d4
baraja25 <92746513+baraja25@users.noreply.github.com>
2024-09-30 16:57:33 +0200

    ejemplo de referencia

--- servidor/tema1/ejemplos/funciones.php
+++ servidor/tema1/ejemplos/funciones.php
@@ -1,14 +1,14 @@
 <?php
-$num = 10;
+
 // function Ejemplo() {
-    
+
 //     global $num; //accede a la variable globl
 
 //     for ($i=0; $i < $num; $i++) { 
 //         echo "Hola";
 //     }
-    
-    
+
+
 
 
 
@@ -16,22 +16,41 @@ $num = 10;
 
 
 
-function potencia($base, $exponente) {
-    $potencia=1;
+// function potencia($base, $exponente) {
+//     $potencia=1;
 
-    for ($i=0; $i < $exponente; $i++) { 
-        $potencia*=$base;
-    }
-    return $potencia;
-}
+//     for ($i=0; $i < $exponente; $i++) { 
+//         $potencia*=$base;
+//     }
+//     return $potencia;
+// }
 
-$base=2;
-$exponente=3;
+// $base=2;
+// $exponente=3;
 
-potencia($base, $exponente);
+// potencia($base, $exponente);
 
-echo "el resultado de la potencia de $base elevado a $exponente es: ", potencia($base, $exponente);
+// echo "el resultado de la potencia de $base elevado a $exponente es: ", potencia($base, $exponente);
 
 //Ejemplo();
 
-?>
+$numero = 15;
+
+$numDecremento = 5;
+
+$valorDecremento = 2;
+
+echo "El numero original es: $numero";
+
+function decrementar(&$numero, $numDecremento, $valorDecremento)
+{
+    while ($numDecremento >= 0) {
+        $numero = $numero - $valorDecremento;
+        $numDecremento--;
+    }
+}
+
+
+decrementar($numero, $numDecremento, $valorDecremento);
+
+echo "Decrementado $numDecremento veces en $valorDecremento resulta en: $numero ";
