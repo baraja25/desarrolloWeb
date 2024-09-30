73fe1bc8e7af3b9d61fdd1c26a183b668a1d72c7
baraja25 <92746513+baraja25@users.noreply.github.com>
2024-09-30 16:14:52 +0200

    recorrer array multidimensional con tablas

--- servidor/tema1/ejemplos/busquedaEjemplo.php
+++ servidor/tema1/ejemplos/busquedaEjemplo.php
@@ -1,6 +1,6 @@
 <?php
 
-//arrays multidimensionales
+//arrays multidimensionales declaracion y recorrido
 
 $alumnos = array(
     "06459782h " => array("juan", "perez", "Lopez"),
@@ -8,12 +8,19 @@ $alumnos = array(
     "05789412x " => array("Maria", "Ruiz", "Ruiz")
 );
 
+echo "<table border='2'>";
+echo "<th>Dni</th><th>Nombre</th><th>Apellido1</th><th>Apellido2</th>";
 
 foreach ($alumnos as $key => $alumno) {
-    echo "El alumno con dni clave: ", $key;
+    
+    echo "<tr>";
+
+    echo "<td>$key</td>";
 
     foreach ($alumno as $key => $campo) {
-        echo $campo, " ";
+        echo "<td>$campo</td>";
     }
-    echo "<br>";
+    echo "</tr>";
 }
+
+echo "</table>";
