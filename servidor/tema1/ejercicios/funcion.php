781312c99e2bcdba0929455acf057e7041f7e28e
baraja25 <92746513+baraja25@users.noreply.github.com>
2024-10-01 14:14:24 +0200

    ejercicio php

--- servidor/tema1/ejercicios/funcion.php
+++ servidor/tema1/ejercicios/funcion.php
@@ -3,65 +3,110 @@ hacer una pagina con un despegable que pida numero de filas y columnas del 1 al 
 para mañana crear un campo de texto que busque el numero dentro de la matriz y que diga en la posicion en la que esta, hay que guardar la matriz
 
 <html>
-<?php 
-$ini=0;
-$fin=0;
-if (isset($_GET['Rellenar'])) {
-    $columna=$_GET["ini"];
-    $row=$_GET["fin"];
-}
+<?php
+$filas = "";
+$columnas = "";
 
 
 ?>
+
 <body>
     <form action="<?php echo $_SERVER["PHP_SELF"] ?>" method="get">
-        <select name="ini">
-        <option value=""></option>
-        <?php
-        for ($i = 0; $i <= 10; $i++) {
-            echo "<option value='$i' ";
-            if ($i == $ini) {
-                echo " selected ";
+        <label for="fila">Filas: </label>
+        <select name="filas" id="filas">
+            <option value=""></option>
+            <?php
+            for ($i = 0; $i <= 10; $i++) {
+                echo "<option value='$i' ";
+                if ($i == $filas) {
+                    echo " selected ";
+                }
+                echo ">$i</option>";
             }
-            echo ">$i</option>";
-        }
-        ?>
+            ?>
         </select>
-        <select name="fin">
-        <option value=""></option>
-        <?php
-        for ($i = 0; $i <= 10; $i++) {
-            echo "<option value='$i' ";
-            if ($i == $fin) {
-                echo " selected ";
+        <label for="columnas">Columnas: </label>
+        <select name="columnas" id="columnas">
+            <option value=""></option>
+            <?php
+            for ($i = 0; $i <= 10; $i++) {
+                echo "<option value='$i' ";
+                if ($i == $columnas) {
+                    echo " selected ";
+                }
+                echo ">$i</option>";
             }
-            echo ">$i</option>";
-        }
-        ?>
+            ?>
         </select>
-        <input type="submit" value="Rellenar">
+        <input type="submit" value="Rellenar" name="rellenar">
     </form>
-        <table border="2">
-    <?php 
-    
-    
-    if (!empty($ini) && !empty($fin)) {
-        for ($i = $ini; $i <= $fin; $i++) {
-            echo "<table border='2'>";
-            echo "<th colspan='2'>Tabla del $i</th>";
-            echo "<tr><td>Operacion</td><td>Resultado</td></tr>";
-            for ($j = 1; $j <= 10; $j++) {
-                echo "<tr><td>$i X $j = </td><td>" . ($i * $j) . "</td></tr>"; //imprimir resultados en cada row
+
+    <?php
+
+    if (isset($_GET['rellenar'])) {
+        $filas = $_GET["filas"];
+        $columnas = $_GET["columnas"];
+        $matriz = [];
+
+
+        //generar matriz y guardarla como cadena
+        for ($i = 0; $i < $filas; $i++) {
+            $fila = [];
+            for ($j = 0; $j < $columnas; $j++) {
+                $fila[] = rand(1, 50);
             }
-            echo "</table>";
+            $matriz[] = implode(",", $fila);
         }
-    } else {
-        echo "Los valores no pueden estar vacios";
+        $matrizString = implode(";", $matriz);
+
+
+        //mostrar tabla
+
+        echo "<table border='2'>";
+
+        foreach ($matriz as $filaString) {
+            echo "<tr>";
+            $valores = explode(",", $filaString);
+            foreach ($valores as $valor) {
+                echo "<td>$valor</td>";
+            }
+            echo "</tr>";
+        }
+        echo "</table>";
+    }
+    ?>
+    <form action="<?php echo $_SERVER["PHP_SELF"] ?>" method="get">
+        <input type="hidden" name="matriz" value="<?php $matrizString  ?>">
+        <label for="numero">Numero a buscar</label>
+        <input type="number" name="numero" id="numero">
+        <input type="submit" name="buscar" value="buscar numero">
+    </form>
+    <?php 
+    if (isset($_GET['buscar'])) {
+        $numero = $_GET['numero'];
+        $matrizString = $_GET['matriz'];
+        $encontrado = false;
+
+        //dividir en filas
+        $filas = explode(";", $matrizString);
+
+        foreach ($filas as $i => $filaString) {
+            $columnas = explode(",", $filaString);
+
+            foreach ($columnas as $j => $valor) {
+                if ($valor == $numero) {
+                    echo "El numero se encuentra en la posicion $i, $j";
+                    $encontrado = true;
+                    break 2;
+                }
+            }
+        }
+
     }
     
     
+    
     ?>
-    </table>
 </body>
 
 </html>
