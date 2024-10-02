4085448e969c9e9812cda3ccd147ffea032de42b
baraja25 <92746513+baraja25@users.noreply.github.com>
2024-10-02 17:13:33 +0200

    prueba de como se usaria con un for

--- diseño/tema 1/ejemplos/regex.js
+++ diseño/tema 1/ejemplos/regex.js
@@ -1,6 +1,22 @@
-//const input = document.getElementsByTagName("input");
-const inputs = document.querySelectorAll("input");
+const inputs = document.getElementsByTagName("input");
+//const inputs = document.querySelectorAll("input");
 
+for (let i = 0; i < inputs.length; i++) {
+    const input = inputs[i];
+    
+    input.addEventListener("input", function () {
+        if (!input.checkValidity()) {
+            input.classList.add("invalid");
+            input.classList.remove("valid");
+        } else {
+            input.classList.remove("invalid");
+            input.classList.add("valid");
+        }
+    })
+}
+
+
+/*
 inputs.forEach(input => {
     input.addEventListener("input", () => {
         if (!input.checkValidity()) {
@@ -12,3 +28,4 @@ inputs.forEach(input => {
         }
     });
 });
+*/
