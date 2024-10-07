3b34913251d79566dd7b3baec5f195e447580f4e
baraja25 <92746513+baraja25@users.noreply.github.com>
2024-10-07 16:06:50 +0200

    añadido formulario del ejercicio

--- servidor/tema1/ejercicios/cadenaBuscada.php
+++ servidor/tema1/ejercicios/cadenaBuscada.php
@@ -9,20 +9,19 @@ function stringpos($aguja, $pajar)
 
 
     //recorrer la cadena principal
-    for ($i = 0; $i <= ($longitudPajar - $longitudAguja); $i++) {
+    for ($i = 0; $i <= ($longitudPajar); $i++) {
         $coincidencia = true;
 
         //comprueba que los caracteres coinciden
         for ($j = 0; $j < $longitudAguja; $j++) {
-            echo "Comparando letra $aguja[$j] de aguja con " . $pajar[($i + $j)] . " de pajar <br>";
+           // echo "Comparando letra $aguja[$j] de aguja con " . $pajar[($i + $j)] . " de pajar <br>";
 
-            if ($aguja[$j] !== $pajar[$i + $j]) {//si los caracteres no coinciden sale del  bucle
+            if ($aguja[$j] !== $pajar[$i + $j]) { //si los caracteres no coinciden sale del  bucle
                 $coincidencia = false;
                 break;
             }
-
         }
-        if ($coincidencia) {//si la variable se mantiene en true devuelve la posicion
+        if ($coincidencia) { //si la variable se mantiene en true devuelve la posicion
             return $i;
         }
     }
@@ -30,11 +29,27 @@ function stringpos($aguja, $pajar)
     return false;
 }
 
-$palabra = "abracadabra";
-$buscar = "cada";
-$pos = stringpos($buscar, $palabra);
-
-echo "la palabra $buscar se encuentra en la posicion $pos";
-
-
 ?>
+
+
+<html>
+
+<body>
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="get">
+        <input type="text" name="principal" id="principal">
+        <input type="text" name="contenida" id="contenida">
+        <input type="submit" value="Buscar" name="Buscar">
+        <?php
+        if (isset($_GET['Buscar'])) {
+            $palabra = $_GET['principal'];
+            $buscar = $_GET['contenida'];
+            $pos = stringpos($buscar, $palabra);
+            echo "la palabra $buscar se encuentra en la posicion $pos";
+        }
+
+
+        ?>
+    </form>
+</body>
+
+</html>
