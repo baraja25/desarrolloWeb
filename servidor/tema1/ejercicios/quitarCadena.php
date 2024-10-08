5a2413ec0f89bcceb3025d5a2dd34a9ea7c67bac
baraja25 <92746513+baraja25@users.noreply.github.com>
2024-10-08 16:37:01 +0200

    crear funcion servidor

--- servidor/tema1/ejercicios/quitarCadena.php
+++ servidor/tema1/ejercicios/quitarCadena.php
@@ -1,5 +1,37 @@
-<?php 
-/* Quitar de la cadena principal una parte por ejemplo hola mundo -> hola añadir un radio para usar varias veces o no */
+<?php
 
+//funcion que indica si esa posicion esta dentro de los limites de la cadena
+function DentroCadena($cad, $pos)
+{
+    return ( ($pos>=0) && ($pos<strlen($cad)) );
+}
+
+
+function extraerCad($cad, $ini, $fin)
+{
+    $sub="";
+    if (DentroCadena($cad, $ini) && DentroCadena($cad, $fin)) {
+        if ($ini > $fin) {
+            echo "<b>Error</b> ha introducido un valor inicial mayor que el final";
+        } else {
+            for ($i = $ini; $i <= $fin; $i++) {
+                $sub .= $cad[$i];
+            }
+        }
+    } else {
+        echo "Error los limites introducidos no estan dentro de la cadena";
+    }
+
+    return $sub;
+}
+
+
+$cadena="telescopio";
+
+$ini = 6;
+$fin = 9;
+$sub = extraerCad($cadena, $ini, $fin);
+echo $sub;
 
 ?>
+
