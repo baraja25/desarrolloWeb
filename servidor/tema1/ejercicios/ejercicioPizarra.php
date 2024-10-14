c2f1d57e1559c3bacfce6332cebc1c21b35664b8
baraja25 <92746513+baraja25@users.noreply.github.com>
2024-10-14 17:19:18 +0200

    ejercicio php terminado

--- servidor/tema1/ejercicios/ejercicioPizarra.php
+++ servidor/tema1/ejercicios/ejercicioPizarra.php
@@ -3,6 +3,84 @@
 ?>
 
 <?php
+
+function mostrarAgenda($agenda)
+{
+    $lineas = explode(",", $agenda);
+    echo "<table border='2'>";
+    echo "<tr>";
+    echo "<th>DNI</th>";
+    echo "<th>Nombre</th>";
+    echo "<th>apellido 1</th>";
+    echo "<th>apellido 2</th>";
+    echo "</tr>";
+
+    foreach ($lineas as $key => $linea) {
+        echo "<tr>";
+
+        $campos = explode(":", $linea);
+
+        foreach ($campos as $key => $campo) {
+
+            echo "<td>$campo</td>";
+        }
+
+        echo "</tr>";
+    }
+}
+
+function lineasNif($agenda) {
+
+    $lineasnif = array();
+
+    $lineas=explode(",", $agenda);
+
+    foreach ($lineas as $key => $linea) {
+        
+        $campos = explode(":", $linea);
+
+        $linea = $lineasnif($campos[0]) ;
+
+    }
+    return $lineasnif;
+
+}
+
+
+function actualizar($linea, $agenda) {
+    $lineasDni = lineasNif($agenda);
+
+    $campos = explode(":",$linea);
+
+    $nif = $campos[0];
+
+    $lineasNif[$nif] = $linea;
+
+    $agenda = implode(",", $lineasNif);
+
+    return $agenda;
+}
+
+function estaAgenda($nif, $agenda)
+{
+    $encontrado = false;
+
+    $lineas = explode(",", $agenda);
+
+    foreach ($lineas as $key => $linea) {
+
+
+        $campos = explode(":", $linea);
+
+        if ($nif == $campos[0]) {
+            $encontrado = true;
+            break;
+        }
+    }
+    return $encontrado;
+}
+
+
 $agenda = "";
 
 if (isset($_GET['agenda'])) {
@@ -11,12 +89,25 @@ if (isset($_GET['agenda'])) {
 
 if (isset($_GET['guardarNombre'])) {
 
+    $nif = $_GET['nif'];
     $nombre = $_GET['nombre'];
+    $apellido1 = $_GET['apellido1'];
+    $apellido2 = $_GET['apellido2'];
+    $linea = $nif . ":" . $nombre . ":" . $apellido1 . ":" . $apellido2; //variable que guarda la linea que vamos a insertar 
 
     if ($agenda == "") {
-        $agenda = $agenda . $nombre;
+        $agenda = $agenda . $linea;
     } else {
-        $agenda = $agenda . "," . $nombre;
+
+        if (!estaAgenda($nif, $agenda)) {
+
+            $agenda = $agenda . "," . $linea;
+        } else {
+
+            //echo "El nif $nif ya existe en la agenda. No se puede volver a insertar";
+
+           $agenda =  actualizar($linea, $linea);
+        }
     }
 }
 
@@ -29,9 +120,18 @@ if (isset($_GET['guardarNombre'])) {
 
 <body>
     <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="get">
-
-        <label for="nombres">Nombre: </label>
+        <label for="nif">NIF: </label>
+        <input type="text" name="nif" id="nif">
+        <br>
+        <label for="nombre">Nombre: </label>
         <input type="text" name="nombre" id="nombre">
+        <br>
+        <label for="apellido1">Apellido: </label>
+        <input type="text" name="apellido1" id="apellido1">
+        <br>
+        <label for="apellido2">Segundo apellido: </label>
+        <input type="text" name="apellido2" id="apellido2">
+        <br>
         <?php
         echo "<input type='hidden' name='agenda' value=$agenda>";
         ?>
@@ -41,23 +141,9 @@ if (isset($_GET['guardarNombre'])) {
     </form>
     <?php
     if (isset($_GET['MostrarTabla'])) {
-
-
-        echo "<table border='2'>";
-        echo "<tr>";
-        echo "<th>Nombre</th>";
-        echo "<th>apellido 1</th>";
-        echo "<th>apellido 2</th>";
-        echo "<th>DNI</th>";
-        echo "</tr>";
-
-        $agendaArray = explode(",", $agenda);
-
-        foreach ($agendaArray as $nombre) {
-            echo "<td>" . $nombre . "</td>";
-        }
+        mostrarAgenda($agenda);
     }
-    //phpinfo();
+
     ?>
 
 
