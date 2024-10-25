4afb60831237d1ad47e6f84ad3cb283d1f170c62
baraja25 <92746513+baraja25@users.noreply.github.com>
2024-10-25 20:09:55 +0200

    subida ejercicio 2

--- cliente/tema4/ejercicios/practica 2/practica.js
+++ cliente/tema4/ejercicios/practica 2/practica.js
@@ -1,4 +1,4 @@
-class vehicle {
+class Vehicle {
   constructor(serial, brand, model, price, taxPercentage, description) {
     if (new.target === vehicle) {
       throw new Error("No se puede instanciar una clase abstracta");
@@ -31,7 +31,7 @@ class vehicle {
   }
 }
 
-class Car extends vehicle {
+class Car extends Vehicle {
   constructor(
     serial,
     brand,
@@ -60,7 +60,7 @@ class Car extends vehicle {
   }
 }
 
-class Motorcycle extends vehicle {
+class Motorcycle extends Vehicle {
   constructor(
     serial,
     brand,
@@ -87,7 +87,7 @@ class Motorcycle extends vehicle {
   }
 }
 
-class Truck extends vehicle {
+class Truck extends Vehicle {
   constructor(
     serial,
     brand,
@@ -108,4 +108,52 @@ class Truck extends vehicle {
       this.loadCapacity
     } toneladas, Número de ejes: ${this.axles}`;
   }
+}
+
+class inventory {
+    constructor(){
+        this.vehicles = [];
+    }
+
+    addVehicle(vehicle) {
+        if (vehicle instanceof Vehicle) {
+            this.vehicles.push(vehicle);
+            console.log(`Vehiculo añadido: ${vehicle.toString()}`)
+        } else {
+            console.log("Solo se pueden añadir vehiculos");
+        }
+    }
+
+    removeVehicle(serial) {
+        const index = this.vehicles.findIndex(vehicle => vehicle.serial === serial);
+        if (index !== -1) {
+            const removedVehicle = this.vehicles.splice(index, 1);
+            console.log(`Vehiculo eliminado: ${removedVehicle[0].toString()}`)
+        } else {
+            console.log("No se encontro un vehiculo con el numero de serie introducido")
+        }
+    }
+
+    listVehicles() {
+        if (this.vehicles.length === 0) {
+            console.log("El inventario esta vacio.");
+        } else {
+            console.log("Vehiculos en el inventario: ");
+            this.vehicles.forEach(vehicle => {
+                console.log(vehicle.toString());
+            });
+        }
+    }
+
+    searchVehicle() {
+        const vehicle = this.vehicles.find(vehicle => vehicle.serial === serial);
+        if (vehicle) {
+            console.log(`Vehiculo encontrado: ${vehicle.toString()}`)
+        } else {
+            console.log("No se encontro el vehiculo.")
+        }
+    }
+
+
+
 }
