79cacf7de18a7d7e49d26bcc08af71f550a43a0a
baraja25 <92746513+baraja25@users.noreply.github.com>
2024-11-08 16:46:33 +0100

    añadida funcion para actualizar en masa

--- servidor/tema2/ejerciciosResueltos/ActAlumno.php
+++ servidor/tema2/ejerciciosResueltos/ActAlumno.php
@@ -1,138 +1,143 @@
 <html>
 
-<?php 
+<?php
 
-$host="localhost";
+$host = "localhost";
 
-$user="root";
+$user = "root";
 
-$pass="";
+$pass = "";
 
-$base="Prueba";
+$base = "tema2";
 
-$db=mysqli_connect($host,$user,$pass,$base);   //Nos conectamos a ese servidor de base de datos y recibimos el descriptor de conexio
+$db = mysqli_connect($host, $user, $pass, $base);   //Nos conectamos a ese servidor de base de datos y recibimos el descriptor de conexio
 
-if (isset($_POST['Actualizar']) )  //si le hemos dado a actualizar alumno
+function MostrarForm($nif)
 {
-    $nif=$_POST['NIF'];
-    $nombre=$_POST['Nombre'];
-    $apellido1=$_POST['Apellido1'];
-    $apellido2=$_POST['Apellido2'];
-    $telefono=$_POST['Telefono'];
-    
-    $consulta="update Alumnos set Nombre='$nombre',
-                                  Apellido1='$apellido1',
-                                  Apellido2='$apellido2',
-                                  Telefono='$telefono'
-                              where NIF='$nif' ";
-    
-    
-    $resul=mysqli_query($db, $consulta);
-    
-    if ($resul)
-    {
-        echo "Registro actualizado correctamente";
+    global $alumnos; //accedemos al array que tiene los datos de los alumnos
+
+
+    echo "<fieldset><legend>Datos del alumno</legend> ";
+
+    echo "NIF<input type='text' name='NIF' value='$nif' readonly='readonly'><br>";
+
+    $fila = $alumnos[$nif];
+
+    echo "Nombre<input type='text' name='Nombres[$nif]' value='$fila[Nombre]'><br>";
+    echo "Apellido1<input type='text' name='Apellidos1[$nif]' value='$fila[Apellido1]'><br>";
+    echo "Apellido2<input type='text' name='Apellidos2[$nif]' value='$fila[Apellido2]'><br>";
+
+    echo "Telefono<input type='text' name='Telefonos[$nif]' value='$fila[telefono]'><br>";
+
+    echo "</fieldset>";
+}
+
+if (isset($_POST['Actualizar']))  //si le hemos dado a actualizar alumno
+{
+
+    $nombres = $_POST['Nombres'];
+    $apellidos1 = $_POST['Apellidos1'];
+    $apellidos2 = $_POST['Apellidos2'];
+    $telefonos = $_POST['Telefonos'];
+
+    foreach ($nombres as $key => $value) {
+        $consulta = "update Alumnos set Nombre='$nombres[$key]',
+                                  Apellido1='$apellidos1[$key]',
+                                  Apellido2='$apellidos2[$key]',
+                                  Telefono='$telefonos[$key]'
+                                    where NIF='$key' ";
+        $resul = mysqli_query($db, $consulta);
     }
-    else
-    {
+
+
+
+
+
+    if ($resul) {
+        echo "Registro actualizado correctamente";
+    } else {
         echo mysqli_error($db);
     }
-    
 }
 
 //Obtenemos los alumos para el desplegable
 
 
-$alumnos=array();
+$alumnos = array();
 
-$consulta="select * from Alumnos";
+$consulta = "select * from Alumnos";
 
-$resul=mysqli_query($db, $consulta);
+$resul = mysqli_query($db, $consulta);
 
 if ($resul)  //Si ha resultado
 {
-   while($alumno=mysqli_fetch_assoc($resul) ) 
-   {
-       $alumnos[$alumno['NIF']]=$alumno;    
-   }
-    
+    while ($alumno = mysqli_fetch_assoc($resul)) {
+        $alumnos[$alumno['NIF']] = $alumno;
+    }
+} else {
+    echo "Error:" . mysqli_error($db);
 }
-else 
-{
-  echo "Error:".mysqli_error($db);  
-}
-
+/*
 $alu="";  //Inicilizamos la variable alu para el NIF del alumno seleccionado
 
 if (isset($_POST['Alumno']) )  //Comprobamos si hay un alumnos previamente seleccionado
 {
     $alu=$_POST['Alumno'];
 }
+*/
 
 
 
-
-mysqli_close($db);  
+mysqli_close($db);
 
 ?>
 
- <body>
+<body>
 
- <fieldset><legend>Seleccione alumno</legend> 
-      <form name="f1" method="post" action="<?php echo $_SERVER['PHP_SELF']  ?>">
-       
-       Alumno<select name='Alumno'>
-                <option value=''></option>
-       
-                <?php 
-                
-                foreach ($alumnos as $alumno)
-                {
-                   echo "<option value='$alumno[NIF]' "; 
-                    
-                   if ($alu==$alumno['NIF'])
-                   {
-                     echo " selected ";  
-                   }
-                       
-                   echo ">$alumno[Apellido1],$alumno[Nombre]</option>";
-                   
-                }
-                
-                ?>
-       
-              </select> 
-       
-       
-        <input type="submit" name="Mostrar" value="Mostrar">
-      
-    </fieldset>  
-      
-        <?php 
-        
-        
-        if (isset($_POST['Mostrar']) )  //Comprobamos si hay un alumnos previamente seleccionado
-        {
-           echo "<fieldset><legend>Datos del alumno</legend> ";
-        
-           echo "NIF<input type='text' name='NIF' value='$alu' readonly='readonly'><br>";
-           
-           $fila=$alumnos[$alu];
-           
-           echo "Nombre<input type='text' name='Nombre' value='$fila[Nombre]'><br>";
-           echo "Apellido1<input type='text' name='Apellido1' value='$fila[Apellido1]'><br>";
-           echo "Apellido2<input type='text' name='Apellido2' value='$fila[Apellido2]'><br>";
-           
-           echo "Telefono<input type='text' name='Telefono' value='$fila[Telefono]'><br>";
-           
-           echo "<input type='submit' name='Actualizar' value='Actualizar'>";
-          
-           echo "</fieldset>";
+    <fieldset>
+        <legend>Seleccione alumno</legend>
+        <form name="f1" method="post" action="<?php echo $_SERVER['PHP_SELF']  ?>">
+
+
+
+            <?php
+
+            echo "<table border='2'>";
+            echo "<th>Selec</th><th>Alumno</th>";
+
+            foreach ($alumnos as $alumno) {
+                echo "<tr>";
+                echo "<td><input type='checkbox' name='Selec[$alumno[NIF]]'></td><td></td><td>$alumno[Apellido1], $alumno[Nombre]</td>";
+
+
+                echo "</tr>";
+            }
+            echo "</table>";
+            ?>
+
+
+
+
+            <input type="submit" name="Mostrar" value="Mostrar">
+            <input type='submit' name='Actualizar' value='Actualizar'>
+    </fieldset>
+
+    <?php
+
+
+    if (isset($_POST['Mostrar']) && (isset($_POST['Selec'])))  //Comprobamos si hay un alumnos previamente seleccionado
+    {
+        $select = $_POST['Selec'];
+
+        foreach ($select as $key => $value) {
+            MostrarForm($key);
         }
-        
-     
-        ?>
-         
-   </form>
- </body> 
-</html> 
+    }
+
+
+    ?>
+
+    </form>
+</body>
+
+</html>
