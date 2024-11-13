ee190f1bd679f842a606de3e232406be47edc335
baraja25 <92746513+baraja25@users.noreply.github.com>
2024-11-13 20:47:24 +0100

    subida ejercicio json

---
+++ cliente/tema4/ejercicios/json/ejercicio.js
@@ -0,0 +1,12 @@
+const persona = {
+    nombre: "Juan",
+    edad: 25,
+    profesion: "Desarrollador"
+};
+
+const jsonPersona = JSON.stringify(persona);
+
+console.log(jsonPersona);
+
+const jsonString = '{"nombre": "Ana", "edad": 30, "ciudad": "Madrid"}';
+
