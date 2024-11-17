7d94fa8d3feb6c7ccbb80e488328d0ed63b1a19d
baraja25 <92746513+baraja25@users.noreply.github.com>
2024-11-17 20:54:38 +0100

    clase lavadora creada

--- cliente/tema4/ejercicios/ejemploExamen/practicaExamen1.js
+++ cliente/tema4/ejercicios/ejemploExamen/practicaExamen1.js
@@ -21,3 +21,18 @@ class Electrodomestico {
         return `Marca: ${this.marca}, Modelo: ${this.modelo}, Precio: ${this.precio}, ConsumoEnergetico: ${this.consumoEnergetico}`;
     }
 }
+
+class Lavadora extends Electrodomestico {
+    constructor(marca, modelo, precio, consumoEnergetico, capacidad) {
+        super(marca, modelo, precio, consumoEnergetico);
+        this.capacidad = capacidad;
+    }
+
+    iniciarLavado() {
+        if (this.encendido) {
+            console.log("El ciclo de lavado ha comenzado.");
+        } else {
+            console.log("La lavadora esta apagada. Por favor enciendela");
+        }
+    }
+}
