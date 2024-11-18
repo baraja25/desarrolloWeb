b708980d111c4b5c11c0a57e1131d04526bd1393
baraja25 <92746513+baraja25@users.noreply.github.com>
2024-11-18 13:56:26 +0100

    clase televesion creada, añadido getters y setters

--- cliente/tema4/ejercicios/ejemploExamen/practicaExamen1.js
+++ cliente/tema4/ejercicios/ejemploExamen/practicaExamen1.js
@@ -1,10 +1,10 @@
 class Electrodomestico {
     constructor(marca, modelo, precio, consumoEnergetico) {
-    this.marca = marca;
-    this.modelo = modelo;
-    this.precio = precio;
-    this.consumoEnergetico = consumoEnergetico;
-    this.encendido = false;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.precio = precio;
+        this.consumoEnergetico = consumoEnergetico;
+        this.encendido = false;
     }
 
     encender() {
@@ -22,11 +22,60 @@ class Electrodomestico {
     }
 }
 
+/* Clase que hereda de electrodomestico*/
+
 class Lavadora extends Electrodomestico {
     constructor(marca, modelo, precio, consumoEnergetico, capacidad) {
         super(marca, modelo, precio, consumoEnergetico);
-        this.capacidad = capacidad;
+        this._marca = marca;
+        this._modelo = modelo;
+        this._precio = precio;
+        this._consumoEnergetico = consumoEnergetico;
+        this._capacidad = capacidad;
     }
+
+
+    get marca() {
+        return this._marca;
+    }
+
+    set marca(value) {
+        this._marca = value;
+    }
+
+    get modelo() {
+        return this._modelo;
+    }
+
+    set modelo(value) {
+        this._modelo = value;
+    }
+
+    get precio() {
+        return this._precio;
+    }
+
+    set precio(value) {
+        this._precio = value;
+    }
+
+    get consumoEnergetico() {
+        return this._consumoEnergetico;
+    }
+
+    set consumoEnergetico(value) {
+        this._consumoEnergetico = value;
+    }
+
+    get capacidad() {
+        return this._capacidad;
+    }
+
+    set capacidad(value) {
+        this._capacidad = value;
+    }
+
+    /* Metodos */
 
     iniciarLavado() {
         if (this.encendido) {
@@ -35,4 +84,71 @@ class Lavadora extends Electrodomestico {
             console.log("La lavadora esta apagada. Por favor enciendela");
         }
     }
+
+
+}
+
+/* Clase que hereda de electrodomestico */
+
+class Televisor extends Electrodomestico {
+    constructor(marca, modelo, precio, consumoEnergetico, pulgadas) {
+        super(marca, modelo, precio, consumoEnergetico);
+        this._marca = marca;
+        this._modelo = modelo;
+        this._precio = precio;
+        this._consumoEnergetico = consumoEnergetico;
+        this._pulgadas = pulgadas;
+    }
+
+    /* getters y setters */
+    get marca() {
+        return this._marca;
+    }
+
+    set marca(value) {
+        this._marca = value;
+    }
+
+    get modelo() {
+        return this._modelo;
+    }
+
+    set modelo(value) {
+        this._modelo = value;
+    }
+
+    get precio() {
+        return this._precio;
+    }
+
+    set precio(value) {
+        this._precio = value;
+    }
+
+    get consumoEnergetico() {
+        return this._consumoEnergetico;
+    }
+
+    set consumoEnergetico(value) {
+        this._consumoEnergetico = value;
+    }
+
+    get pulgadas() {
+        return this._pulgadas;
+    }
+
+    set pulgadas(value) {
+        this._pulgadas = value;
+    }
+
+    /* Metodos */
+
+    cambiarCanal() {
+        if (this.encendido) {
+            console.log("Has cambiado de canal.");
+        } else {
+            console.log("El televisor esta apagado.");
+        }
+    }
+
 }
