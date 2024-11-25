5e19df65816bae4c368a34414b851a66fd64610a
baraja25 <92746513+baraja25@users.noreply.github.com>
2024-11-25 20:10:01 +0100

    animacion de sonic sacada de internet y un ejemplo de que la imagen rebote

---
+++ diseño/tema 3/ejemplos/animaciones/animacionCustom.js
@@ -0,0 +1,37 @@
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
+const image = new Image();
+image.src = "ibai.png";
+image.width = 100;
+image.height = 110;
+
+let x = 100; //posicion inicial
+let y = 100; //posicion inicial
+let dx = 5; //velocidad inicial
+let dy = 5; //velocidad inicial
+
+
+image.onload = function () {
+    requestAnimationFrame(actualizar);
+}
+
+function actualizar() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height); // limpiar la imagen
+    ctx.drawImage(image, x, y); //dibujar la imagen
+
+
+    //actualizar la posicion
+    x += dx;
+    y += dy;
+
+    //comprobar colision
+    if (x + image.width > canvas.width || x < 0) {
+        dx = -dx; // cambiar la direccion en el eje x
+    }
+
+    if (y + image.height > canvas.height || y < 0) {
+        dy = -dy; // cambiar la direccion en el eje y
+    }
+
+    requestAnimationFrame(actualizar);
+}
