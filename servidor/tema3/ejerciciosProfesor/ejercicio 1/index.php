9ca6ea3151e4609ac878a7ac37c2516c57a297eb
baraja25 <92746513+baraja25@users.noreply.github.com>
2024-12-09 17:19:14 +0100

    subida primer ejercicio profesor

---
+++ servidor/tema3/ejerciciosProfesor/ejercicio 1/index.php
@@ -0,0 +1,71 @@
+<?php
+require_once("libreria.php");
+//funciones
+function obtenerMarcas() {
+    $datosObtenidos = array();
+    $consulta = "SELECT * FROM marcas";
+    $datosObtenidos = consultaDatos($consulta);
+    return $datosObtenidos;
+}
+
+//inicializacion de variables
+$marca = isset($_POST['marcas']) ? $_POST['marcas'] : ""; 
+$nombre = isset($_POST['nombre']) ? $_POST['nombre'] : "";
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 1</title>
+</head>
+
+<body>
+    <form name="f1" action="<?php echo $_SERVER["PHP_SELF"] ?>" method="post" enctype="multipart/form-data">
+        Nombre<input type="text" name="nombre"><br>
+        Marcas <select name="marcas" onchange="f1.submit();">
+            <option value=""></option>
+        <?php 
+        $marcasObtenidas = obtenerMarcas();
+        foreach ($marcasObtenidas as $key => $value) {
+            echo "<option value='$value[id]' ";
+            if ($marca === $marcasObtenidas[0]['id']) {
+                echo " selected ";
+                $imagen = $value['Imagen'];
+            }
+            echo " >$value[Nombre]</option>";
+        }
+        
+        ?>
+        </select>
+        <img src='img/marcas/<?php echo $imagen?>' width="70" height="70">
+        <br>
+        Modelo<input type="text" name="modelo"><br>
+        Precio<input type="number" name="precio"><br>
+        Foto<input type="file" name="foto"><br>
+        <?php 
+        if (isset($_POST['Guardar'])) {
+            $modelo = $_POST['modelo'];
+            $precio = $_POST['precio'];
+
+
+            if ($_FILES['Foto']['name']!="") {
+                $nombreFoto = $_FILES['Foto']['name'];
+                $nombreTemp = $_FILES['Foto']['tmp_name'];
+
+                copy($nombreTemp, "img/vehiculos/$nombreFoto");
+            }
+
+            $consulta = "INSERT INTO coches VALUES(null, $nombre, $marca, $modelo, $nombreFoto)";
+            consultaSimple($consulta);
+        }
+        
+        ?>
+        <input type="submit" value="Guardar" name="guardar">
+    </form>
+</body>
+
+</html>
