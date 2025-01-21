5a1651bceaeb32ec95a8b8709a9269dcbd291ebb
baraja25 <92746513+baraja25@users.noreply.github.com>
2025-01-21 13:21:12 +0100

    primer examen de recuperacion

--- servidor/tema1/simulacro examen/recuperacionPracticas/1/encuesta.view.php
+++ servidor/tema1/simulacro examen/recuperacionPracticas/1/encuesta.view.php
@@ -1,21 +1,32 @@
-<!doctype html>
-<html lang="es">
+<!DOCTYPE html>
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Encuesta</title>
 </head>
 <body>
-<form action="<?php echo $_SERVER["PHP_SELF"] ?>">
+<h2>Encuesta de Satisfacción</h2>
+<form method="POST" action=" <?php echo $_SERVER["PHP_SELF"] ?>" name="f1">
+    <!-- Pregunta de género -->
+    <h3>Selecciona tu género:</h3>
+    <label><input type="radio" name="Genero" value="Masculino" checked> Masculino</label><br>
+    <label><input type="radio" name="Genero" value="Femenino"> Femenino</label><br>
+    <label><input type="radio" name="Genero" value="No especificar"> No especificar</label><br>
 
-    <input type="radio" name="Genero" id="Genero" value="Masculino">
-    <label for="Genero">Masculino</label>
-    <input type="radio" name="Genero" id="Genero" value="Femenino">
-    <label for="Genero">Femenino</label>
-    <input type="radio" name="Genero" id="Genero" value="No especificar">
-    <label for="Genero">No especificar</label>
+    <!-- Comentarios -->
+    <h3>¿Qué opinas sobre el servicio?</h3>
+    <textarea name="Opinion" rows="4" cols="50" placeholder="Escribe tu opinión aquí..."></textarea><br>
+
+    <!-- Preferencias -->
+    <h3>¿Cuáles son tus aficiones?</h3>
+    <label><input type="checkbox" name="Aficiones[]" value="Leer"> Leer</label><br>
+    <label><input type="checkbox" name="Aficiones[]" value="Deportes"> Deportes</label><br>
+    <label><input type="checkbox" name="Aficiones[]" value="Viajar"> Viajar</label><br>
+
+    <!-- Botón enviar -->
+    <button type="submit" name="enviar">Enviar Encuesta</button>
+    <?php
+    require "encuesta.php";
+    ?>
 </form>
 </body>
 </html>
