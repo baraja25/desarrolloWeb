f08369aaf25d21f5caab7bb9b187b32449be8cb6
baraja25 <92746513+baraja25@users.noreply.github.com>
2025-02-18 12:30:46 +0100

    añadida funcion para ordenar array

--- servidor/guarreov2/sesiones101/1prof/productoSesion.php
+++ servidor/guarreov2/sesiones101/1prof/productoSesion.php
@@ -1,5 +1,23 @@
 <?php
 require_once 'producto.php';
+
+
+function sortArray(&$array, $sortKey) {
+    usort($array, function($a, $b) use ($sortKey) {
+        // Verificamos si $a y $b son objetos
+        if (is_object($a) && is_object($b)) {
+            return strcmp($a->__get($sortKey), $b->__get($sortKey));
+        }
+        // Verificamos si $a y $b son arrays asociativos
+        elseif (is_array($a) && is_array($b)) {
+            return strcmp($a[$sortKey], $b[$sortKey]);
+        }
+        // Si no son del mismo tipo, puedes manejarlo como desees
+        return 0; // o puedes lanzar un error
+    });
+}
+
+
 session_start();
 
 if (isset($_POST['insertar'])) {
@@ -40,6 +58,16 @@ if (isset($_POST['actualizar'])) {
 if (isset($_POST['vaciar'])) {
     unset($_SESSION['productos']);
 }
+
+// Ordenar productos
+if (isset($_GET['sort'])) {
+    $sort = $_GET['sort'];
+    
+    sortArray($_SESSION['productos'], $sort);
+}
+
+// Mostrar tabla si hay productos en la sesión o si se ha hecho clic en "Mostrar"
+$mostrarTabla = isset($_POST['mostrar']) || isset($_GET['sort']);
 ?>
 
 <!DOCTYPE html>
@@ -64,29 +92,27 @@ if (isset($_POST['vaciar'])) {
             <input type="submit" value="Borrar" name="borrar">
         </fieldset>
         <?php
-        if (isset($_POST['mostrar'])) {
-            if (isset($_SESSION['productos'])) {
-                echo "<table border='1'>
-                <tr>
-                    <th>Selecciona</th>
-                    <th>ID</th>
-                    <th>Nombre</th>
-                    <th>Marca</th>
-                    <th>Precio</th>
-                </tr>";
+        if ($mostrarTabla && isset($_SESSION['productos'])) {
+            echo "<table border='1'>
+            <tr>
+                <th>Selecciona</th>
+                <th><a href='?sort=id'>ID</a></th>
+                <th><a href='?sort=nombre'>Nombre</a></th>
+                <th><a href='?sort=marca'>Marca</a></th>
+                <th><a href='?sort=precio'>Precio</a></th>
+            </tr>";
 
-                foreach ($_SESSION['productos'] as $producto) {
-                    echo "<tr>
-                    <td><input type='checkbox' name='borrar_ids[" . $producto->__get('id') . "]'></td>
-                    <td><input type='text' name='actualizar_ids[" . $producto->__get('id') . "][id]' value='" . $producto->__get('id') . "' readonly></td>
-                    <td><input type='text' name='actualizar_ids[" . $producto->__get('id') . "][nombre]' value='" . $producto->__get('nombre') . "'></td>
-                    <td><input type='text' name='actualizar_ids[" . $producto->__get('id') . "][marca]' value='" . $producto->__get('marca') . "'></td>
-                    <td><input type='text' name='actualizar_ids[" . $producto->__get('id') . "][precio]' value='" . $producto->__get('precio') . "'></td>
-                  </tr>";
-                }
-
-                echo "</table>";
+            foreach ($_SESSION['productos'] as $producto) {
+                echo "<tr>
+                <td><input type='checkbox' name='borrar_ids[" . $producto->__get('id') . "]'></td>
+                <td><input type='text' name='actualizar_ids[" . $producto->__get('id') . "][id]' value='" . $producto->__get('id') . "' readonly></td>
+                <td><input type='text' name='actualizar_ids[" . $producto->__get('id') . "][nombre]' value='" . $producto->__get('nombre') . "'></td>
+                <td><input type='text' name='actualizar_ids[" . $producto->__get('id') . "][marca]' value='" . $producto->__get('marca') . "'></td>
+                <td><input type='text' name='actualizar_ids[" . $producto->__get('id') . "][precio]' value='" . $producto->__get('precio') . "'></td>
+              </tr>";
             }
+
+            echo "</table>";
             echo "<input type='submit' value='Actualizar' name='actualizar'>";
             echo "<input type='submit' value='Vaciar' name='vaciar'>";
         }
